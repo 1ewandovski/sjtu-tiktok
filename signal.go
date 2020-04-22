03646d158d741bba178d2adea2932bfe80bddf39
greenhandatsjtu <sunhengke@sjtu.edu.cn>
2020-04-22 16:52:27 +0800

    add a Go file
    Took 15 minutes

---
+++ signal.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"golang.org/x/sys/unix"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func main() {
+	ch := make(chan os.Signal)
+	signal.Notify(ch, os.Kill, os.Interrupt, syscall.SIGUSR1, syscall.SIGUSR2)
+	go func() {
+		sig := <-ch
+		fmt.Println("Receive signal: ", sig)
+		fmt.Println("Exiting...")
+		fmt.Println("Clearing...")
+		close(ch)
+		os.Exit(0)
+	}()
+	fmt.Println("Waiting for signal...")
+	_ = unix.Pause()
+}
